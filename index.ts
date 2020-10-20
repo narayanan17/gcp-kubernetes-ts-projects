0d4f3ea1534121f7925b09a869cd61fb669ebf0b
narayanan17 <73112749+narayanan17@users.noreply.github.com>
2020-10-20 17:52:47 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -113,7 +113,7 @@ const deployment = new k8s.apps.v1.Deployment(name,
                         {
                             name: name,
                             image: "ruchi672/todo-microservice:latest",
-                            ports: [{ name: "http", containerPort: 8080 }],
+                            ports: [{ name: "http", containerPort: 80 }],
                         },
                     ],
                 },
